048c09ce850740130973ded9cf7e78dd5991fc71
MarkiianPerliak <perliak2012@gmail.com>
2025-09-06 21:27:42 +0300

    done

---
+++ src/js/projects.js
@@ -0,0 +1,11 @@
+// Template:
+// <li class="projects__project-item">
+//     <img src="" alt="Project Photo" class="project__photo">
+//     <p class="project__programming-language"></p>
+//     <div class="flex-box project-data">
+// <p class="project__name"></p>
+//     <button class="project__visit-button">Visit<svg class="project__arrow"></svg></button>
+//     </div>
+// </li>
+
+// <button class="load-more-projects">Load more <div class="under-line"></div></button>
