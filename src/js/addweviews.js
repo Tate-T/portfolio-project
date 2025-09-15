8fa54065679b477ac8a9fdea03c7ab3ed0f939b7
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2025-09-15 18:31:49 +0300

    adapt

---
+++ src/js/addweviews.js
@@ -0,0 +1,17 @@
+  const form = document.querySelector('.addrewiews__form');
+  const modal = document.getElementById('cooperationModal');
+  const closeBtn = document.querySelector('.modal__close');
+  const overlay = document.querySelector('.modal__overlay');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault(); // зупиняємо відправку форми
+    modal.classList.add('active');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+
+  overlay.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
